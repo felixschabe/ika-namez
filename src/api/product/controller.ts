cfe7a0a056c1550b4871972b326088113a95e258
hello@felixschabe.com <Felix.Schabe@edeka.de>
2022-01-09 15:00:00 +0000

    refactoring

--- src/api/product/controller.ts
+++ src/api/product/controller.ts
@@ -2,22 +2,24 @@ import { ProductRepository } from "./repository";
 import { bind } from "decko";
 import { Request, Response } from "express";
 import { getRandom } from "../../services/random";
+import { createDefaultApiResponse } from "../../utils/utils";
 
 export class ProductController {
   readonly repo: ProductRepository = new ProductRepository();
 
   @bind
   async getProduct(req: Request, res: Response): Promise<Request | void> {
-    const productsJson = await this.repo.getProducts();
+    let productsJson = await this.repo.getProducts();
     const startsWith = req.query.startsWith;
 
     if (startsWith) {
-      const startsWithProducts = productsJson.filter((product: any) =>
+      productsJson = productsJson.filter((product: any) =>
         product.name.toUpperCase().startsWith((startsWith + "").toUpperCase())
       );
-      const random = await getRandom(startsWithProducts.length);
-      res.json(startsWithProducts[random]).status(200);
-      return;
+    }
+
+    if (productsJson.length <= 0) {
+      return createDefaultApiResponse(res, true, "nothing found", 404);
     }
 
     const random: number = await getRandom(productsJson.length);
