0c7afacef9a8e5dd8859e257962b94775ff8b583
hello@felixschabe.com <Felix.Schabe@edeka.de>
2022-01-09 15:00:00 +0000

    added indexing products

---
+++ README.md
@@ -0,0 +1,19 @@
+# IKEA-MOEBEL-NAME
+***
+This helps you!  
+Basically it lists some names of ikea products.
+
+Branches:  
+
+|Branch    |Description         |
+|----------|--------------------|
+|api       |getting ikea names  |
+|index     |indexing process    |
+|ui        |userinterface       |
+
+# INFO 
+
+ - This uses random.org. It generates true random numbers by atmospheric noise
+```
+https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new  
+```
---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import fetch from "node-fetch";
+import { writeFileSync } from "fs";
+
+indexNames().then((ikeaProducts: IkeaProduct[]) => {
+  console.log(
+    "we are now filtering " +
+      ikeaProducts.length +
+      " item(s) for unique names.. this may or may not take a while"
+  );
+
+  const uniqueIkeaProducts = ikeaProducts.filter((ikeaProduct: IkeaProduct) => {
+    const firstIkeaProduct = ikeaProducts.find((element: IkeaProduct) => {
+      return ikeaProduct.name === element.name;
+    });
+    return ikeaProduct === firstIkeaProduct;
+  });
+
+  console.log(
+    "we`ve found " + uniqueIkeaProducts.length + " item(s) with unique names"
+  );
+
+  writeFileSync(
+    "output/ikeaProducts.json",
+    JSON.stringify(uniqueIkeaProducts, null, 2)
+  );
+});
+
+async function indexNames(): Promise<IkeaProduct[]> {
+  const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+  const IKEA_API_URL: URL = new URL(
+    "https://sik.search.blue.cdtapps.com/de/de/search-result-page?max-num-filters=8&q=b&autocorrect=true&size=99999&columns=4&store=&subcategories-style=tree-navigation&types=PRODUCT%2CCONTENT%2CPLANNER%2CREFINED_SEARCHES%2CANSWER&c=sr&v=20211021"
+  );
+
+  const ikeaProducts: IkeaProduct[] = [];
+
+  for (let alphabetNumber = 0; alphabetNumber < 26; alphabetNumber++) {
+    const letter: string = ALPHABET.charAt(alphabetNumber);
+    const ikeaQuery: URL = IKEA_API_URL;
+    ikeaQuery.searchParams.set("q", letter);
+    console.log("Fetching: " + ikeaQuery.href);
+    await fetch(ikeaQuery.href)
+      .then((response: any) => response.json())
+      .then((data: any) => {
+        const items = data.searchResultPage.products.main.items;
+        for (let element of items) {
+          const product: any | null = element.product || null;
+          if (product) {
+            const id = product.id || "";
+            const priceNumeral = product.priceNumeral || "";
+            const name = product.name || "";
+            const typeName = product.typeName || "";
+            const pipUrl = product.pipUrl || "";
+
+            ikeaProducts.push(
+              new IkeaProduct(id, priceNumeral, name, typeName, pipUrl)
+            );
+          }
+        }
+      });
+  }
+
+  return ikeaProducts;
+}
+
+class IkeaProduct {
+  public id: string;
+  public priceNumeral: number;
+  public name: string;
+  public typeName: number;
+  public pipUrl: string;
+
+  constructor(
+    id: string,
+    priceNumeral: number,
+    name: string,
+    typeName: number,
+    pipUrl: string
+  ) {
+    this.id = id;
+    this.priceNumeral = priceNumeral;
+    this.name = name;
+    this.typeName = typeName;
+    this.pipUrl = pipUrl;
+  }
+}
